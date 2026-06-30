7b03d95d2abb8870be3a13533025f9f88b0b24bb
SamuvelRaja <josephsamuvel001@gmail.com>
2026-06-30 10:13:48 +0530

    fixed build issue

--- app/designwebsite/template7/[slug]/robots.ts
+++ app/designwebsite/template7/[slug]/robots.ts
@@ -1,6 +1,12 @@
 import { MetadataRoute } from 'next';
+import { getAllSlugs } from '@/lib/dataBuilder';
 
 export const dynamic = 'force-static';
+
+export async function generateStaticParams() {
+  const slugs = await getAllSlugs();
+  return slugs.map((slug) => ({ slug }));
+}
 
 export default function robots(): MetadataRoute.Robots {
   return {
